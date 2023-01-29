9e6dfe411cdf2174702d3e36c58ec10f53dab20c
Trevor Ramdhan <tramdhan@users.noreply.github.com>
2023-01-29 10:40:10 -0500

    Create south_american_countries.js

---
+++ south_american_countries.js
@@ -0,0 +1,76 @@
+const south_american_countries = [
+  {
+    id: 1,
+    country: "Argentina",
+    alpha2: "AR",
+    alpha3: "ARG",
+  },
+  {
+    id: 2,
+    country: "Bolivia",
+    alpha2: "BO",
+    alpha3: "BOL",
+  },
+  {
+    id: 3,
+    country: "Brazil",
+    alpha2: "BR",
+    alpha3: "BRA",
+  },
+  {
+    id: 4,
+    country: "Chile",
+    alpha2: "CL",
+    alpha3: "CHL",
+  },
+  {
+    id: 5,
+    country: "Colombia",
+    alpha2: "CO",
+    alpha3: "COL",
+  },
+  {
+    id: 6,
+    country: "Ecuador",
+    alpha2: "EC",
+    alpha3: "ECU",
+  },
+  {
+    id: 7,
+    country: "Guyana",
+    alpha2: "GY",
+    alpha3: "GUY",
+  },
+  {
+    id: 8,
+    country: "Paraguay",
+    alpha2: "PY",
+    alpha3: "PRY",
+  },
+  {
+    id: 9,
+    country: "Peru",
+    alpha2: "PE",
+    alpha3: "PER",
+  },
+  {
+    id: 10,
+    country: "Suriname",
+    alpha2: "SR",
+    alpha3: "SUR",
+  },
+  {
+    id: 11,
+    country: "Uruguay",
+    alpha2: "UY",
+    alpha3: "URY",
+  },
+  {
+    id: 12,
+    country: "Venezuela",
+    alpha2: "VE",
+    alpha3: "VEN",
+  },
+];
+
+// The Falkland Islands are not considered to be in South America. They are an overseas territory of the United Kingdom located in the South Atlantic Ocean.
