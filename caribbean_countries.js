793fcd9b025062a7e1ebe684cc96354fc968117c
Trevor Ramdhan <tramdhan@users.noreply.github.com>
2023-01-29 10:48:39 -0500

    Create caribbean_countries.js

---
+++ caribbean_countries.js
@@ -0,0 +1,80 @@
+const caribbean_countries = [
+  {
+    id: 1,
+    country: "Antigua and Barbuda",
+    alpha2: "AG",
+    alpha3: "ATG",
+  },
+  {
+    id: 2,
+    country: "Bahamas",
+    alpha2: "BS",
+    alpha3: "BHS",
+  },
+  {
+    id: 3,
+    country: "Barbados",
+    alpha2: "BB",
+    alpha3: "BRB",
+  },
+  {
+    id: 4,
+    country: "Cuba",
+    alpha2: "CU",
+    alpha3: "CUB",
+  },
+  {
+    id: 5,
+    country: "Dominica",
+    alpha2: "DM",
+    alpha3: "DMA",
+  },
+  {
+    id: 6,
+    country: "Dominican Republic",
+    alpha2: "DO",
+    alpha3: "DOM",
+  },
+  {
+    id: 7,
+    country: "Grenada",
+    alpha2: "GD",
+    alpha3: "GRD",
+  },
+  {
+    id: 8,
+    country: "Haiti",
+    alpha2: "HT",
+    alpha3: "HTI",
+  },
+  {
+    id: 9,
+    country: "Jamaica",
+    alpha2: "JM",
+    alpha3: "JAM",
+  },
+  {
+    id: 10,
+    country: "Saint Kitts and Nevis",
+    alpha2: "KN",
+    alpha3: "KNA",
+  },
+  {
+    id: 11,
+    country: "Saint Lucia",
+    alpha2: "LC",
+    alpha3: "LCA",
+  },
+  {
+    id: 12,
+    country: "Saint Vincent and the Grenadines",
+    alpha2: "VC",
+    alpha3: "VCT",
+  },
+  {
+    id: 13,
+    country: "Trinidad and Tobago",
+    alpha2: "TT",
+    alpha3: "TTO",
+  },
+];
